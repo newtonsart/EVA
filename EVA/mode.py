7517fbe92b2dd40e56268ae572618c8be8d829a8
LolArt <artsecurity@protonmail.com>
2019-10-21 17:55:37 +0200

    Update mode.py

--- EVA/mode.py
+++ EVA/mode.py
@@ -3,6 +3,8 @@ import say, json, wikipedia
 change_commands = {"en":"change","es":"cambiar"}
 exit_commands = {"en":"exit", "es":"salir"}
 search_commands = {"en":"search", "es":"buscar"}
+silence_commands = {"en":"silence", "es":"silencio"}
+say_commands = {"en":"say", "es":"di"}
 
 def change_language(language):
     if language == "Spanish":
@@ -59,14 +61,38 @@ def get_func(speech, lang):
                 elif lang == "es":
                     say.Spanish("Sé más preciso")
             
+        elif mode == silence_commands[lang]:
+            with open("info.json", "r") as read_file:
+                data = json.load(read_file)
+                silence = data["silence"]
+            
+            if silence == "False":
+                with open("info.json", "w") as write_file:
+                    data["silence"] = "True"
+                    write_file.write(json.dumps(data))
+
+            if silence == "True":
+                with open("info.json", "w") as write_file:
+                    data["silence"] = "False"
+                    write_file.write(json.dumps(data))
+
+        elif mode == say_commands[lang]:
+            if lang == "en":
+                say.English(" ".join(speech_divided[1::]))
+            elif lang == "es":
+                say.Spanish(" ".join(speech_divided[1::]))
 
         else: 
-            print("Strange input: \n"+speech)
-            if lang == "en":
-                say.English("I haven't understood you")
+            with open("info.json", "r") as read_file:
+                data = json.load(read_file)
+                silence = data["silence"]
+            if silence == "False":
+                print("Strange input: \n"+speech)
+                if lang == "en":
+                    say.English("I haven't understood you")
 
-            elif lang == "es":
-                say.Spanish("No te he entendido bien")
+                elif lang == "es":
+                    say.Spanish("No te he entendido bien")
 
     except IndexError: pass
     except UnboundLocalError: pass
